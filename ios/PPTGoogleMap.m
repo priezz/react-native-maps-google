a47278c1a6392f8eeea9b20dc59fd9759f377a90
Yonah Forst <Yonah@Yonahs-MacBook-Pro-2.local>
2016-02-05 00:25:57 +0100

    code style

--- ios/PPTGoogleMap.m
+++ ios/PPTGoogleMap.m
@@ -102,8 +102,9 @@
         
         GMSMarker* mapMarker = [GMSMarker markerWithPosition:CLLocationCoordinate2DMake(latitude, longitude)];
         
-        if (marker[@"icon"])
+        if (marker[@"icon"]) {
             mapMarker.icon = [self getMarkerImage:marker];
+        }
         
         mapMarker.userData = publicId;
         mapMarker.map = self;
