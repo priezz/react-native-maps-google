7716ab3ad015400c26a1430a84cbe6dc2f1d80ba
Thuong <dinhthuong02@hotmail.com>
2016-06-23 15:46:31 +0700

    Upgrade latest Google SDK

--- sdk.sh
+++ sdk.sh
@@ -2,8 +2,8 @@
 
 mkdir -p ios_modules
 cd ios_modules
-curl "https://www.gstatic.com/cpdc/c0e534927c0c955e-GoogleMaps-1.11.1.tar.gz" -o "GoogleMaps-1.11.1.tar.gz"
-rm -rf GoogleMaps-1.11.1
-mkdir GoogleMaps-1.11.1
-tar -zxf GoogleMaps-1.11.1.tar.gz -C GoogleMaps-1.11.1
-rm GoogleMaps-1.11.1.tar.gz
+curl "https://www.gstatic.com/cpdc/aa3052925ceeea2d-GoogleMaps-1.13.2.tar.gz" -o "GoogleMaps-1.13.2.tar.gz"
+rm -rf GoogleMaps-1.13.2
+mkdir GoogleMaps-1.13.2
+tar -zxf GoogleMaps-1.13.2.tar.gz -C GoogleMaps-1.13.2
+rm GoogleMaps-1.13.2.tar.gz
